cf0792a03f9ecc9d43b364473e17584eecb5f7bd
ErodedElk <303504852@qq.com>
2022-04-03 20:00:31 +0800

    sunday

---
+++ week3/inspect-fds/src/process.rs
@@ -0,0 +1,102 @@
+use crate::open_file::OpenFile;
+use std::fs;
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Process {
+    pub pid: usize,
+    pub ppid: usize,
+    pub command: String,
+}
+
+impl Process {
+    pub fn new(pid: usize, ppid: usize, command: String) -> Process {
+        Process { pid, ppid, command }
+    }
+
+
+    pub fn print(&self)
+    {
+        println!("============== {} (pid {}, ppid {}) =============", self.command, self.pid, self.ppid);
+        match self.list_open_files() {
+            None => println!(
+                "Warning: could not inspect file descriptors for this process! \
+                    It might have exited just as we were about to look at its fd table, \
+                    or it might have exited a while ago and is waiting for the parent \
+                    to reap it."
+            ),
+            Some(open_files) => {
+                for (fd, file) in open_files {
+                    println!(
+                        "{:<4} {:<15} cursor: {:<4} {}",
+                        fd,
+                        format!("({})", file.access_mode),
+                        file.cursor,
+                        file.colorized_name(),
+                    );
+                }
+            }
+        }
+    }
+    /// This function returns a list of file descriptor numbers for this Process, if that
+    /// information is available (it will return None if the information is unavailable). The
+    /// information will commonly be unavailable if the process has exited. (Zombie processes
+    /// still have a pid, but their resources have already been freed, including the file
+    /// descriptor table.)
+    pub fn list_fds(&self) -> Option<Vec<usize>> {
+        let dir=format!("/proc/{}/fd",self.pid);
+        let mut fds = Vec::<usize>::new();
+        for entry in fs::read_dir(dir).ok()?{
+            let entry = entry.ok()?;
+            let filename=entry.file_name().into_string().ok()?.parse::<usize>().ok()?;
+            fds.push(filename);
+        }
+        Some(fds)
+    }
+
+    /// This function returns a list of (fdnumber, OpenFile) tuples, if file descriptor
+    /// information is available (it returns None otherwise). The information is commonly
+    /// unavailable if the process has already exited.
+    pub fn list_open_files(&self) -> Option<Vec<(usize, OpenFile)>> {
+        let mut open_files = vec![];
+        for fd in self.list_fds()? {
+            open_files.push((fd, OpenFile::from_fd(self.pid, fd)?));
+        }
+        Some(open_files)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::ps_utils;
+    use std::process::{Child, Command};
+
+    fn start_c_program(program: &str) -> Child {
+        Command::new(program)
+            .spawn()
+            .expect(&format!("Could not find {}. Have you run make?", program))
+    }
+
+    #[test]
+    fn test_list_fds() {
+        let mut test_subprocess = start_c_program("./multi_pipe_test");
+        let process = ps_utils::get_target("multi_pipe_test").unwrap().unwrap();
+        assert_eq!(
+            process
+                .list_fds()
+                .expect("Expected list_fds to find file descriptors, but it returned None"),
+            vec![0, 1, 2, 4, 5]
+        );
+        let _ = test_subprocess.kill();
+    }
+
+    #[test]
+    fn test_list_fds_zombie() {
+        let mut test_subprocess = start_c_program("./nothing");
+        let process = ps_utils::get_target("nothing").unwrap().unwrap();
+        assert!(
+            process.list_fds().is_none(),
+            "Expected list_fds to return None for a zombie process"
+        );
+        let _ = test_subprocess.kill();
+    }
+}
---
+++ week3/linked_list/src/linked_list.rs
@@ -0,0 +1,144 @@
+use std::fmt;
+use std::option::Option;
+
+pub struct LinkedList<T> {
+    head: Option<Box<Node<T>>>,
+    size: usize,
+}
+
+struct Node<T> {
+    value: T,
+    next: Option<Box<Node<T>>>,
+}
+
+impl<T> Node<T> {
+    pub fn new(value: T, next: Option<Box<Node<T>>>) -> Node<T> {
+        Node {value: value, next: next}
+    }
+}
+
+impl<T> LinkedList<T> {
+    pub fn new() -> LinkedList<T> {
+        LinkedList {head: None, size: 0}
+    }
+    
+    pub fn get_size(&self) -> usize {
+        self.size
+    }
+    
+    pub fn is_empty(&self) -> bool {
+        self.get_size() == 0
+    }
+    
+    pub fn push_front(&mut self, value: T) {
+        let new_node: Box<Node<T>> = Box::new(Node::new(value, self.head.take()));
+        self.head = Some(new_node);
+        self.size += 1;
+    }
+    
+    pub fn pop_front(&mut self) -> Option<T> {
+        let node: Box<Node<T>> = self.head.take()?;
+        self.head = node.next;
+        self.size -= 1;
+        Some(node.value)
+    }
+}
+
+
+impl<T:fmt::Display> fmt::Display for LinkedList<T> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let mut current: &Option<Box<Node<T>>> = &self.head;
+        let mut result = String::new();
+        loop {
+            match current {
+                Some(node) => {
+                    result = format!("{} {}", result, node.value);
+                    current = &node.next;
+                },
+                None => break,
+            }
+        }
+        write!(f, "{}", result)
+    }
+}
+
+impl<T> Drop for LinkedList<T> {
+    fn drop(&mut self) {
+        let mut current = self.head.take();
+        while let Some(mut node) = current {
+            current = node.next.take();
+        }
+    }
+}
+impl<T:Clone> Clone for Node<T>{
+    fn clone(&self)->Self
+    {
+        Node{value:self.value.clone(),next:self.next.clone()}
+    }
+}
+
+impl<T:Clone> Clone for LinkedList<T>{
+    fn clone(&self)->Self
+    {
+        LinkedList{head:self.head.clone(),size:self.size.clone()}
+    }
+}
+
+impl <T:PartialEq> PartialEq for Node<T> {
+    fn eq(&self,other:&Node<T>)->bool{
+        (self.value==other.value)&&(self.next==other.next)
+    }
+}
+
+impl <T:PartialEq> PartialEq for LinkedList<T> {
+    fn eq(&self,other:&LinkedList<T>)->bool{
+        (self.head==other.head)&&(self.size==other.size)
+    }
+}
+
+pub trait ComputeNorm{
+    fn compute_norm(&self)->f64
+    {0.0}
+}
+
+impl ComputeNorm for LinkedList<f64> {
+    fn compute_norm(&self)->f64{
+        let mut sq=0.0;
+        for i in self {
+            sq+=i*i;
+        }
+        sq.sqrt()
+    } 
+}
+
+impl<T> Iterator for LinkedList<T> {
+    type Item = T;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.pop_front()
+    }
+}
+
+pub struct LinkedListIter<'a, T> {
+    current: &'a Option<Box<Node<T>>>,
+}
+impl<T: Clone> Iterator for LinkedListIter<'_, T> {
+    type Item = T;
+    fn next(&mut self) -> Option<T> {
+        match self.current {
+            Some(node) => {
+                self.current = &node.next;
+                Some(node.value.clone())
+            },
+            None => {
+                None
+            }
+        }
+    }
+}
+impl<'a, T: Clone> IntoIterator for &'a LinkedList<T> {
+    type Item = T;
+    type IntoIter = LinkedListIter<'a, T>;
+    fn into_iter(self) -> LinkedListIter<'a, T> {
+        LinkedListIter {current: &self.head}
+    }
+}
